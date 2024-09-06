40dc70448dc33cd87aad1c0efa93ba5e486118e7
Kevin Albert <146172986+TheFifthNinja@users.noreply.github.com>
2024-09-06 17:38:28 +1200

    added file import for testing

--- frontend/tests/main.test.jsx
+++ frontend/tests/main.test.jsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import App from '@/App';
 
 describe('Hero Tests', () => {
   it('always passes', () => {
